f8d906aea5ddb4448b6fbc941e74a1a88849407b
haswinsk <skhaswin@gmail.com>
2025-06-14 20:01:44 +0530

    app.js modified: message changed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.get("/",(req,res) => {
-    res.send("welcome to shopeasy!");
+    res.send("welcome to shopeasy! from jvl code");
 })
 
 app.listen(PORT, () => {
